a376c5cad53d555c380e15922c2db151b0ab157d
Adam <adambokun@gmail.com>
2021-11-15 11:58:05 +0300

    Add project

---
+++ SteamList/SteamList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SteamList
+//
+//  Created by Adam Bokun on 15.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+}
+
