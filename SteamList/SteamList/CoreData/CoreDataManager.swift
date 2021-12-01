99ead9c6e453d8f34b42dbd3a2e1000f1ecaf66a
Adam <adambokun@gmail.com>
2021-12-01 20:03:24 +0300

    implement core data

---
+++ SteamList/SteamList/CoreData/CoreDataManager.swift
@@ -0,0 +1,109 @@
+//
+//  CoreDataManager.swift
+//  SteamList
+//
+//  Created by Adam Bokun on 1.12.21.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager: NSObject {
+
+    private override init() {
+        super.init()
+        applicationLibraryDirectory()
+    }
+
+    static let sharedConst = CoreDataManager()
+
+    class func shared() -> CoreDataManager {
+        return sharedConst
+    }
+
+    private lazy var applicationDocumentsDirectory: URL = {
+        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        print(urls[urls.count-1])
+        return urls[urls.count-1]
+    }()
+
+    private func applicationLibraryDirectory() {
+        print(applicationDocumentsDirectory)
+        if let url = FileManager.default.urls(for: .libraryDirectory, in: .userDomainMask).last {
+            print(url.absoluteString)
+        }
+    }
+    // MARK: - Core Data stack
+
+    lazy var managedObjectContext = {
+
+        return self.persistentContainer.viewContext
+    }()
+
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "GameListDB")
+        container.loadPersistentStores(completionHandler: { (_, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    func prepare(dataForSaving: [Games]) {
+        _ = dataForSaving.map {self.createEntityFrom(games: $0)}
+        saveData()
+    }
+
+    private func createEntityFrom(games: Games) -> GameItems? {
+        let gameItem = GameItems(context: self.managedObjectContext)
+        gameItem.appid = games.appid
+        gameItem.name = games.name
+        
+        return gameItem
+    }
+
+    func saveData() {
+
+        let context = self.managedObjectContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+
+    func saveDataInBackground() {
+        persistentContainer.performBackgroundTask { (context) in
+            if context.hasChanges {
+                do {
+                    try context.save()
+                } catch {
+                    let nserror = error as NSError
+                    fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+                }
+            }
+        }
+    }
+
+    func fetchAllData() -> [Games] {
+        do {
+            let fetchRequest: NSFetchRequest<GameItems>
+            fetchRequest = GameItems.fetchRequest()
+
+            let context = persistentContainer.viewContext
+            let objects = try context.fetch(fetchRequest)
+            let fetchResult = objects.compactMap { game -> Games in
+                return Games(appId: game.appid,
+                             name: game.name ?? "")
+                }
+            return fetchResult
+        } catch {
+            print(error.localizedDescription)
+            return []
+        }
+    }
+}
---
+++ SteamList/SteamList/CoreData/GameItems+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  GameItems+CoreDataClass.swift
+//  SteamList
+//
+//  Created by Adam Bokun on 1.12.21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(GameItems)
+public class GameItems: NSManagedObject {
+
+}
